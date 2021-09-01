15664740797038a115bb761f963140951ed1c58b
Wonno <wonno@live.com>
2021-09-01 20:59:31 +0200

    add LDAP configuration

--- src/main/java/com/weareadaptive/nexus/casc/plugin/internal/config/Config.java
+++ src/main/java/com/weareadaptive/nexus/casc/plugin/internal/config/Config.java
@@ -1,5 +1,7 @@
 package com.weareadaptive.nexus.casc.plugin.internal.config;
 
+import org.sonatype.nexus.ldap.persist.internal.LdapConfigurationData;
+
 import java.util.List;
 
 public class Config {
@@ -7,6 +9,7 @@ public class Config {
     private ConfigRepository repository;
     private ConfigSecurity security;
     private List<ConfigCapability> capabilities;
+    private List<LdapConfigurationData> ldapConfigs;
 
     public ConfigCore getCore() {
         return core;
@@ -39,4 +42,8 @@ public class Config {
     public void setCapabilities(List<ConfigCapability> capabilities) {
         this.capabilities = capabilities;
     }
+
+    public List<LdapConfigurationData> getLdapConfigs() { return ldapConfigs; }
+
+    public void setLdapConfigs(List<LdapConfigurationData> ldapConfigs) { this.ldapConfigs = ldapConfigs; }
 }
